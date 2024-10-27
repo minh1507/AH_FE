f1931cb43071668f5aa730bb03c1e435febe5001
Duong Duc Anh <duongdoican@gmail.com>
2024-10-27 10:36:00 +0700

    uipdate

--- src/service/category.ts
+++ src/service/category.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export class CategoryService{
     static findAll = async () => {
         try {
-            const response = await axios.get('http://222.255.1.152:4600/category', {
+            const response = await axios.get('https://222.255.1.152:4600/category', {
                 params: {
                     limit: 500,
                     offset: 0
