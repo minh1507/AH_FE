5aada1d4f5bfd09c045c55a71c6d3c1f2ff93305
Duong Duc Anh <duongdoican@gmail.com>
2024-10-27 20:40:09 +0700

    upat

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Bind to all available interfaces
+    port: 5173,      // Specify the port you want to use
     hmr: {
-      host: 'anhoangstore.xyz',
-      port: 443, // Use the correct port if necessary
-      protocol: 'wss' // Ensure this matches the secure connection
+      host: 'localhost', // Adjust this for HMR if necessary
+      protocol: 'ws'     // Use 'ws' for non-secure connections
     }
-  },
+  }
 })
