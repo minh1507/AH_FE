9456da97bcdc8a293586a44a52b8445a9b9e1287
Duong Duc Anh <duongdoican@gmail.com>
2024-10-27 10:11:12 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import fs from 'fs'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 4000,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certificates/private-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certificates/certificate.crt'))
-    },
     watch: {
       usePolling: true, 
     },
