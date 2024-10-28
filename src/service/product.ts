264d45545390601932493fbc00e46273a86dcc0e
Anh Duong Duc <anhdd6@officevnpay.com>
2024-10-28 10:43:15 +0700

    update

--- src/service/product.ts
+++ src/service/product.ts
@@ -3,14 +3,14 @@ import axios from 'axios';
 export class ProductService{
     static findAll = async (category: number) => {
         try {
-            const response = await axios.get('https://222.255.1.152:4600/product?categoryId=' + category);
+            const response = await axios.get('https://222.255.1.152:4600/be/product?categoryId=' + category);
             
             const products = response.data.data;
     
             const productsWithImages = await Promise.all(
                 products.map(async (product:any) => {
                     try {
-                        const imageResponse = await axios.get(`https://222.255.1.152:4600/file/${product.file.file}`);
+                        const imageResponse = await axios.get(`https://222.255.1.152:4600/be/file/${product.file.file}`);
                         return { ...product, imageURL: imageResponse.data.data }; 
                     } catch (imageError) {
                         console.log(`Error fetching image for file ${product.file.file}`, imageError);
