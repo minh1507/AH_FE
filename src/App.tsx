3145db8481701270c9945a3b510e21eeefcfa1f6
Duong Duc Anh <duongdoican@gmail.com>
2024-12-04 00:04:09 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import {Suspense} from "react";
 import { ToastProvider } from "./component/toast/toast";
 import PortalModule from "./module/portal.module";
 import { RecoilRoot } from 'recoil';
+import ReactGA from 'react-ga4';
 
 function App() {
+    React.useEffect(() => {
+        ReactGA.initialize("G-ZDZKNNV4W6");
+    
+        ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+      }, []);
+
     return (
         <BrowserRouter>
             <Suspense>
